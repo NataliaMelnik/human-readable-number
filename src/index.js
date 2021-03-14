f078ba8a26d8fa50fa49e88db2767375f6113403
NataliaMelnik <bukato.natallia@gmail.com>
2021-03-14 22:02:28 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let decimalPartNames = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    let numberNames = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+
+    if (number == 0) return "zero";
+
+    if (number < 20) {
+        return numberNames[number];
+    }
+
+    if (number < 100) {
+        let decimalPart = decimalPartNames[Math.floor(number / 10)];
+        return (number % 10 === 0) ? decimalPart : decimalPart + " " + numberNames[number % 10];
+    }
+
+    let hundredthPart = numberNames[Math.floor(number / 100)] + " hundred";
+    return (number % 100 === 0) ? hundredthPart : hundredthPart + " " + toReadable(number % 100);
 }
